cc7eb84196679330c40270aaa3a2cb740c23dfbb
Alfi WebGen ID <wgenesis.pro@gmail.com>
2024-01-04 23:00:52 +0700

    update teacher list

--- COMPONENTS/PARTIALS/Teacher.tsx
+++ COMPONENTS/PARTIALS/Teacher.tsx
@@ -13,9 +13,9 @@ export default function Example() {
                 <ul className="grid grid-cols-2 md:grid-cols-4 gap-12 pt-16">
                     {people.map((person, i) => (
                         <li key={i}>
-                            <div className="flex flex-col lg:flex-row items-center gap-x-6">
-                                <Image className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" width={256} height={256} />
-                                <div>
+                            <div className="grid grid-cols-1 md:grid-cols-3 gap-x-6">
+                                <Image className="h-16 w-16 rounded-full object-cover object-top" src={person.imageUrl} alt="" width={256} height={256} />
+                                <div className="md:col-span-2">
                                     <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
                                     <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
                                 </div>
